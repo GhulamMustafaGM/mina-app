402e0829dd8be7c1087cd8bd0fa9e55c30d014f1
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-26 21:14:21 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <React.StrictMode>
