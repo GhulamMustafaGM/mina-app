0e4c8eefcf6adcd9826e1bb611bb1487a64b9003
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-27 18:18:58 +0200

    updated ShoppingCart

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import Product from "./Product";
 
 export default class ShoppingCart extends Component {
-    state = {
-        products: [
-            { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
-            { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
-            { id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0 },
-            { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
-            { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
-            { id: 6, productName: "Dell Monitor", price: 880, quantity: 0 },
-        ],
-    };
+    //Executes when the component is mounted
+    constructor(props) {
+        //console.log("constructor - ShoppingCart");
+        super(props); //calling super class's constructor
+
+        //initialization of the state
+        this.state = {
+            products: [],
+        };
+    }
 
     render() {
+        //console.log("render - ShoppingCart");
+
         return (
             <div className="container-fluid">
                 <h4>Shopping Cart</h4>
@@ -37,6 +39,51 @@ export default class ShoppingCart extends Component {
         );
     }
     // render ends here
+
+    //Executes after constructor and render method (includes life cycle of child components, if any) of current component
+    componentDidMount = async () => {
+        //send request to server
+        var response = await fetch("http://localhost:5000/products", {
+            method: "GET",
+        });
+
+        //the following code executes after receiving response from server
+        //converting the response body into a JS object array
+        var prods = await response.json();
+
+        //the following code executes after converting response body into JS object array
+        console.log(prods);
+
+        //updating products into component's state
+        this.setState({ products: prods });
+
+        //console.log("componentDidMount - ShoppingCart");
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        /*console.log(
+          "componentDidUpdate - ShoppingCart",
+          prevProps,
+          prevState,
+          this.props,
+          this.state
+        );*/
+        // if (prevProps.x != this.props.x) {
+        //   //make http call
+        // }
+    }
+
+    //Executes when the current instance of current component is being deleted from memory
+    componentWillUnmount() {
+        //console.log("componentWillUnmount - ShoppingCart");
+    }
+
+    componentDidCatch(error, info) {
+        //console.log("componentDidCatch - ShoppingCart");
+        //console.log(error, info);
+
+        localStorage.lastError = `${error}\n${JSON.stringify(info)}`;
+    }
 
     //executes when the user clicks on + button.
     handleIncrement = (product, maxValue) => {
