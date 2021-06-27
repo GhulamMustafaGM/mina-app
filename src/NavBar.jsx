7be746437adbf99234e7d9c21b8ff7fa50d6b52c
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-27 19:33:58 +0200

    updated NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 class NavBar extends Component {
     render() {
@@ -23,17 +24,94 @@ class NavBar extends Component {
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/#">
-                                    Home
-                                </a>
-                            </li>
+                            {!this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                        exact={true}
+                                    >
+                                        Login
+                                    </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/dashboard"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >
+                                        Dashboard
+                                    </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/customers"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >
+                                        Customers
+                                    </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <NavLink
+                                        to="/cart"
+                                        className="nav-link"
+                                        activeClassName="active"
+                                    >
+                                        Shopping Cart
+                                    </NavLink>
+                                </li>
+                            ) : (
+                                ""
+                            )}
+
+                            {this.props.isLoggedIn ? (
+                                <li className="nav-item">
+                                    <a
+                                        href="/#"
+                                        className="nav-link"
+                                        onClick={this.onLogoutClick}
+                                    >
+                                        Logout
+                                    </a>
+                                </li>
+                            ) : (
+                                ""
+                            )}
                         </ul>
                     </div>
                 </nav>
             </React.Fragment>
         );
     }
+
+    //Executes when the user clicks on the Logout link
+    onLogoutClick = (event) => {
+        //prevent the page refresh when the user clicks on Logout link
+        event.preventDefault();
+
+        //update isLoggedInStatus as false
+        this.props.updateIsLoggedInStatus(false);
+
+        //navigate login component
+        document.location.hash = "/";
+    };
 }
 
 export default NavBar;
