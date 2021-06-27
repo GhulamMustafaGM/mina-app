a114aef393fee9c894dbeae58ed94150f438b4ea
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-27 19:29:50 +0200

    updated App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
-//import ShoppingCart from "./ShoppingCart";
+import Dashboard from "./Dashboard";
 import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { Route, Switch } from "react-router";
+import { HashRouter } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
+import SideBar from "./SideBar";
+import ProductByID from "./ProductByID";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <NavBar />
-        <Login></Login>
-      </React.Fragment>
+      <HashRouter>
+        <NavBar
+          isLoggedIn={this.state.isLoggedIn}
+          updateIsLoggedInStatus={this.updateIsLoggedInStatus}
+        />
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-3">
+              {this.state.isLoggedIn ? <SideBar></SideBar> : ""}
+            </div>
+
+            <div className="col-lg-9">
+              <Switch>
+                <Route
+                  path="/"
+                  exact
+                  render={(props) => (
+                    <Login
+                      {...props}
+                      updateIsLoggedInStatus={this.updateIsLoggedInStatus}
+                    />
+                  )}
+                />
+                <Route path="/dashboard" exact component={Dashboard} />
+                <Route path="/customers" exact component={CustomersList} />
+                <Route path="/cart" exact component={ShoppingCart} />
+                <Route path="/product/:id" component={ProductByID} />
+                <Route path="*" component={NoMatchPage} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
+
+  //This method can be called by child components to update isLoggedIn property of the state
+  updateIsLoggedInStatus = (status) => {
+    this.setState({ isLoggedIn: status });
+  };
 }
